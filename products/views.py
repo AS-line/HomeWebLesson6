fabda0b82e1516b86fe980ae0fa01a3250a0b6ea
As-line <32175896+AS-line@users.noreply.github.com>
2018-03-17 03:21:50 +0300

    Add files via upload

--- products/views.py
+++ products/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def index(request):
+    berriers_arr = []
+    fruits_and_veg = []
+    i = 0
+    for j in Product.objects.all():
+        if j.catalog_id == 3:
+            berriers_arr.append(j)
+        if j.catalog_id == 1 or j.catalog_id == 2:
+            fruits_and_veg.append(j)
+        i += j.count
+
     context = {
-        'all_products': Product.objects.all()
-
+        'all_products': Product.objects.all(),
+        "all_count": i,
+        "berriers_arr": berriers_arr,
+        "fruits_and_veg": fruits_and_veg,
     }
-    return render(request, 'index.html', context)
 
+    return render(request, 'index.html', context)
